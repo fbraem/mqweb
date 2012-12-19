97079e917ea4db0db47d12b95e4dba70bf033534
fbraem <franky.braem@gmail.com>
2012-12-19 19:05:19 +0100

    spaces --> tabs

--- MQ/include/MQ/PCF.h
+++ MQ/include/MQ/PCF.h
@@ -35,122 +35,150 @@
 namespace MQ
 {
 
-  class PCF : public Message
-  {
-  public:
+class PCF : public Message
+{
+public:
 
-    explicit PCF(bool zos = false);
-
-
-    explicit PCF(int cmd, bool zos = false);
-    
-    
-    virtual ~PCF();
-
-    
-    void addParameter(MQLONG parameter, const std::string& value);
-    
-
-    void addParameter(MQLONG parameter, MQLONG value);
-    
-    
-    void addParameterList(MQLONG parameter, MQLONG *values);
-    
-    
-    void addFilter(MQLONG parameter, MQLONG op, const std::string& value);
-    
-    
-    void addFilter(MQLONG parameter, MQLONG op, MQLONG value);
-
-    
-    int getCommand() const;
-
-    
-    int getCompletionCode() const;
+explicit PCF(bool zos = false);
 
 
-    int getReasonCode() const;
-
-    
-    bool isExtendedResponse() const;
+explicit PCF(int cmd, bool zos = false);
 
 
-    bool isLast() const;
-
-    
-    std::string getParameterString(MQLONG parameter) const;
-    
-    
-    Poco::DateTime getParameterDate(MQLONG dateParameter, MQLONG timeParameter) const;
-    
-    
-    MQLONG getParameterNum(MQLONG parameter) const;
-    
-    
-    bool hasParameter(MQLONG parameter) const;
-    
-    
-    std::vector<int> getParameters() const;
+virtual ~PCF();
 
 
-    typedef Poco::SharedPtr<PCF> Ptr;
+void addParameter(MQLONG parameter, const std::string& value);
 
 
-    typedef std::vector<Ptr> Vector;
-
-  private:
-
-    std::map<MQLONG, int> _pointers;
+void addParameter(MQLONG parameter, MQLONG value);
 
 
-    bool _zos;
+void addParameterList(MQLONG parameter, MQLONG *values);
 
 
-    PCF(const PCF& pcf);
+void addFilter(MQLONG parameter, MQLONG op, const std::string& value);
 
 
-    void init();
+void addFilter(MQLONG parameter, MQLONG op, MQLONG value);
 
 
-    friend class CommandServer;
-  };
+int getCommand() const;
 
 
-  inline int PCF::getCommand() const 
-  { 
-      MQCFH* header = (MQCFH*)(MQBYTE*) &buffer()[0];
-      return header->Command; 
-  }
+int getCompletionCode() const;
 
-  inline int PCF::getCompletionCode() const 
-  { 
-      MQCFH* header = (MQCFH*)(MQBYTE*) &buffer()[0];
-      return header->CompCode; 
-  }
 
-  inline int PCF::getReasonCode() const 
-  { 
-      MQCFH* header = (MQCFH*)(MQBYTE*) &buffer()[0];
-      return header->Reason; 
-  }
+int getReasonCode() const;
 
-  inline bool PCF::isExtendedResponse() const
-  {
-      MQCFH* header = (MQCFH*)(MQBYTE*) &buffer()[0];
-      return header->Type == MQCFT_XR_SUMMARY; 
-  }
-  
-  
-  inline bool PCF::hasParameter(MQLONG parameter) const
-  {
-    return _pointers.find(parameter) != _pointers.end();
-  }
 
-  inline bool PCF::isLast() const
-  {
-    MQCFH* header = (MQCFH*)(MQBYTE*) &buffer()[0];
-    return header->Control == MQCFC_LAST;
-  }
+bool isExtendedResponse() const;
+
+
+bool isNumber(MQLONG parameter) const;
+
+
+bool isString(MQLONG parameter) const;
+
+
+bool isLast() const;
+
+
+std::string getParameterString(MQLONG parameter) const;
+
+
+Poco::DateTime getParameterDate(MQLONG dateParameter, MQLONG timeParameter) const;
+
+
+MQLONG getParameterNum(MQLONG parameter) const;
+
+
+bool hasParameter(MQLONG parameter) const;
+
+
+std::vector<MQLONG> getParameters() const;
+
+
+typedef Poco::SharedPtr<PCF> Ptr;
+
+
+typedef std::vector<Ptr> Vector;
+
+private:
+
+std::map<MQLONG, int> _pointers;
+
+
+bool _zos;
+
+
+PCF(const PCF& pcf);
+
+
+void init();
+
+
+friend class CommandServer;
+};
+
+
+inline int PCF::getCommand() const 
+{
+	MQCFH* header = (MQCFH*)(MQBYTE*) &buffer()[0];
+	return header->Command; 
+}
+
+inline int PCF::getCompletionCode() const 
+{ 
+	MQCFH* header = (MQCFH*)(MQBYTE*) &buffer()[0];
+	return header->CompCode; 
+}
+
+inline int PCF::getReasonCode() const 
+{ 
+	MQCFH* header = (MQCFH*)(MQBYTE*) &buffer()[0];
+	return header->Reason; 
+}
+
+inline bool PCF::isExtendedResponse() const
+{
+	MQCFH* header = (MQCFH*)(MQBYTE*) &buffer()[0];
+	return header->Type == MQCFT_XR_SUMMARY; 
+}
+
+
+inline bool PCF::hasParameter(MQLONG parameter) const
+{
+	return _pointers.find(parameter) != _pointers.end();
+}
+
+inline bool PCF::isLast() const
+{
+	MQCFH* header = (MQCFH*)(MQBYTE*) &buffer()[0];
+	return header->Control == MQCFC_LAST;
+}
+
+inline bool PCF::isNumber(MQLONG parameter) const
+{
+	std::map<MQLONG, int>::const_iterator it = _pointers.find(parameter);
+	if ( it != _pointers.end() )
+	{
+		MQLONG *pcfType = (MQLONG*) &buffer()[it->second];
+		return *pcfType == MQCFT_INTEGER;
+	}
+	return false;
+}
+
+inline bool PCF::isString(MQLONG parameter) const
+{
+	std::map<MQLONG, int>::const_iterator it = _pointers.find(parameter);
+	if ( it != _pointers.end() )
+	{
+		MQLONG *pcfType = (MQLONG*) &buffer()[it->second];
+		return *pcfType == MQCFT_STRING || *pcfType == MQCFT_BYTE_STRING;
+	}
+	return false;
+}
 
 } // MQ namespace
 
