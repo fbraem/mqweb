a949ac7b40c816330f17df24766f961bef16c1b9
fbraem <franky.braem@gmail.com>
2013-12-25 20:01:43 +0100

    Use AngularJS for queues

---
+++ static/js/app/queues.js
@@ -0,0 +1,65 @@
+var mqWebApp = angular.module('mqWebApp');
+
+mqWebApp.controller('QueuesController', ['$scope', '$http', 'MQWEB_CONFIG', function($scope, $http, config) {
+	$scope.loading = false;
+	$scope.mqweb = null;
+	$scope.queues = null;
+	$scope.error = null;
+	
+	$scope.formData = {
+		queueName : config.queueName ? config.queueName : "*",
+		queueType : config.queueType ? config.queueType : "All",
+		queueExcludeSystem : false,
+		queueExcludeTemp : false
+	};
+
+	$scope.load = function() {
+		$scope.loading = true;
+		$http.get('/queue/list.json/' + config.qmgrName, {
+					cache: false,
+					params : $scope.formData
+					})
+			.success(function(data, status) {
+				$scope.loading = false;
+				$scope.mqweb = data.mqweb;
+				$scope.queues = [];
+				for(q in data.queues)
+				{
+					$scope.queues.push({
+						loading: false,
+						toggle: false,
+						data: data.queues[q]
+					});
+				}
+				$scope.error = data.error;
+			}).error(function(data, status) {
+				$scope.loading = false;
+		});
+	};
+	
+	$scope.reload = function(queue)
+	{
+		queue.loading = true;
+		$http.get('/queue/list.json/' + config.qmgrName,
+			{	cache : false,
+				params: {
+					queueName: queue.data.QName.value,
+					queueType: queue.data.QType.display
+				}
+			})
+			.success(function(data, status) {
+				queue.loading = false;
+				if ( data.queues.length > 0 )
+				{
+					queue.data = data.queues[0];
+				}
+			}).error(function(data, status) {
+				queue.loading = false;
+			});
+	}
+
+	if ( config.autoload )
+	{
+		$scope.load();
+	}
+}]);
--- templates/queue/view_head.tpl
+++ templates/queue/view_head.tpl
@@ -1,47 +1,10 @@
-<script	type="text/javascript">
-var QueueModel = function()
-{
-	var self = this;
-	
-	self.loading = ko.observable(false);
-	self.queue = ko.observable(null);
-	self.error = ko.observable(null);
-	
-	self.reload = function()
-	{
-		$.ajax(
-		{
-			url: "/queue/view.json/<?= mqweb.qmgr ?>/<?= queueName ?>",
-			cache: false,
-			dataType: "json",
-			success: function(data) {
-				self.loading(false);
-				if ( data.error )
-				{
-					self.error(data.error);
-				}
-				else
-				{
-					self.queue(data.queue);
-				}
-			},
-			error: function (request, status, error)
-			{
-				alert(status);
-				//self.loading(false);
-				//TODO: Handle Error on page
-			}
-		});
-		
-		return false;
-	}	
-}
-
-var queueModel = new QueueModel();
-
-$(document).ready(function() {
-	ko.applyBindings(queueModel);
-	queueModel.reload();
-});
+<script type="text/javascript">
+	var mqWebApp = angular.module('mqWebApp');
+	mqWebApp.constant('MQWEB_CONFIG', {
+		qmgrName : '<?= mqweb.qmgr ?>',
+		queueName : '<?= queueName ?>',
+		html: 'queue/queue.html',
+		autoload : true
+	});
 </script>
-<? include "queue.tpl" ?>
+<script type="text/javascript" src="/static/js/app/queues.js"></script>
