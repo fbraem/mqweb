b82d6181647c3597e91781439e125523f61ce1b3
fbraem <franky.braem@gmail.com>
2014-01-07 19:35:02 +0100

    Allow to browse specific message / Add MessageDumpController

--- static/js/app/messages.js
+++ static/js/app/messages.js
@@ -12,7 +12,12 @@ mqWebApp.controller('MessagesController', ['$scope', '$http', 'MQWEB_CONFIG', fu
 
 	$scope.load = function() {
 		$scope.loading = true;
-		$http.get('/message/browse/' + config.qmgrName + '/' + config.queueName, {
+		var url = '/message/browse/' + config.qmgrName + '/' + config.queueName;
+		if ( config.messageId )
+		{
+			url += '/' + config.messageId;
+		}
+		$http.get(url, {
 					cache: false,
 					params : $scope.formData
 					})
@@ -50,6 +55,8 @@ mqWebApp.controller('MessagesController', ['$scope', '$http', 'MQWEB_CONFIG', fu
 				{
 					message.data = data.messages[0];
 				}
+				$scope.error = data.error;
+				$scope.mqweb = data.mqweb;
 			}).error(function(data, status) {
 				message.loading = false;
 				$scope.http_rc = status;
@@ -60,4 +67,33 @@ mqWebApp.controller('MessagesController', ['$scope', '$http', 'MQWEB_CONFIG', fu
 	{
 		$scope.load();
 	}
+}]);
+
+mqWebApp.controller('MessageDumpController', ['$scope', '$http', 'MQWEB_CONFIG', function($scope, $http, config) {
+	$scope.loading = false;
+	$scope.mqweb = null;
+	$scope.message = null;
+	$scope.error = null;
+	$scope.http_rc = 0;
+	$scope.ascii = true;
+	$scope.ebcdic = true;
+	
+	$scope.load = function() {
+		$scope.loading = true;
+		var url = '/message/dump.json/' + config.qmgrName + '/' + config.queueName + '/' + config.messageId;
+		$http.get(url, {
+					cache: false,
+					})
+			.success(function(data, status) {
+				$scope.loading = false;
+				$scope.mqweb = data.mqweb;
+				$scope.message = data.message;
+				$scope.http_rc = 0;
+				$scope.error = data.error;
+			}).error(function(data, status) {
+				$scope.loading = false;
+				$scope.http_rc = status;
+			});
+	};
+	$scope.load();
 }]);
