db3b0abab05fef5fd96ab672517e5bcd14849aab
fbraem <franky.braem@gmail.com>
2014-01-25 22:00:48 +0100

    Add a PHP sample

---
+++ samples/queue.php
@@ -0,0 +1,16 @@
+<?php
+
+	/*
+	 * Inquire all queues from queuemanager PIGEON.
+	 * MQWeb runs on localhost and is listening on port 8081. 
+	 */
+	$url = "http://localhost:8081/queue/inquire/PIGEON";
+	$curl = curl_init();
+	curl_setopt($curl, CURLOPT_URL, $url);
+	curl_setopt($curl, CURLOPT_HEADER, false);
+	curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+	curl_setopt($curl, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1);
+	$response = curl_exec($curl);
+	$data = json_decode($response, true);		
+
+	print_r($data);
