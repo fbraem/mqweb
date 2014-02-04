b761f36d5e2941b0716a0ae124d0a124769a2e94
fbraem <franky.braem@gmail.com>
2014-02-04 22:05:47 +0100

    Add events action to provider

--- static/js/app/message/provider.js
+++ static/js/app/message/provider.js
@@ -11,6 +11,7 @@ angular.module('mqWebApp').provider('mqWebMessage', {
 			getQueue : function() {
 				return queue;
 			},
+			// Browse a message
 			// Pass an object for using the query parameters of browse
 			// or a message id to use the URI path
 			browse : function() {
@@ -21,12 +22,29 @@ angular.module('mqWebApp').provider('mqWebMessage', {
 						});
 				}
 				else {
-						return $http.get('/message/browse/' + qmgr + '/' + queue + '/' + arguments[0], { cache : false } );
+					return $http.get('/message/browse/' + qmgr + '/' + queue + '/' + arguments[0], { cache : false } );
 				}
 			},
 			
+			// Dump a message
+			// Pass the message id as argument
 			dump : function() {
 				return $http.get('/message/dump/' + qmgr + '/' + queue + '/' + arguments[0], { cache : false } );
+			},
+			
+			// Browse event messages
+			// Pass an object for using the query parameters of browse
+			// or a message id to use the URI path
+			events : function() {
+				if ( typeof arguments[0] == 'object' ) {
+					return $http.get('/message/event/' + qmgr + '/' + queue, {
+						cache : false,
+						params : arguments[0]
+						});
+				}
+				else {
+					return $http.get('/message/event/' + qmgr + '/' + queue + '/' + arguments[0], { cache : false });
+				}
 			}
 		}
 	}],
