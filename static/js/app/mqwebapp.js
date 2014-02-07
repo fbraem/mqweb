19da5631095bc36fe6229a8607e9a66109ee9b8c
fbraem <franky.braem@gmail.com>
2014-02-07 20:39:40 +0100

    mqConfig isn't used anymore

--- static/js/app/mqwebapp.js
+++ static/js/app/mqwebapp.js
@@ -1,28 +1,5 @@
 var mqWebApp = angular.module('mqWebApp', ['ngRoute']);
 
-mqWebApp.directive('mqConfig', ['MQWEB_CONFIG', function(config) {
-	return {
-		restrict: 'E',
-		replace: true,
-		link : function(scope, element, attrs) 
-		{ 
-			if ( attrs['name'] )
-			{
-				element.text(config[attrs['name']]);
-			}
-		}
-	};
-}]);
-
-mqWebApp.directive('mqTemplate',
-		['MQWEB_CONFIG',
-			function (config) {
-				return {
-				 restrict: 'E',
-				 templateUrl: '/static/html/' + config['html']
-				};
-}]);
-
 mqWebApp.filter('default', function () {  
 	return function (input, value) {
 		if (input !== null && input !== undefined && (input !== '' || angular.isNumber(input))) {
