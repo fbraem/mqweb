aa71cf66b898b48ffcd38e3ce70b8d83405ca5af
fbraem <franky.braem@gmail.com>
2014-03-09 21:19:08 +0100

    Add ClusterQueueManager

---
+++ MQWeb/src/ClusterQueueManagerController.cpp
@@ -0,0 +1,70 @@
+/*
+ * Copyright 2010 MQWeb - Franky Braem
+ *
+ * Licensed under the EUPL, Version 1.1 or – as soon they
+ * will be approved by the European Commission - subsequent
+ * versions of the EUPL (the "Licence");
+ * You may not use this work except in compliance with the
+ * Licence.
+ * You may obtain a copy of the Licence at:
+ *
+ * http://joinup.ec.europa.eu/software/page/eupl
+ *
+ * Unless required by applicable law or agreed to in
+ * writing, software distributed under the Licence is
+ * distributed on an "AS IS" basis,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied.
+ * See the Licence for the specific language governing
+ * permissions and limitations under the Licence.
+ */
+#include "MQ/Web/MQController.h"
+#include "MQ/Web/ClusterQueueManagerController.h"
+#include "MQ/Web/ClusterQueueManagerMapper.h"
+#include "MQ/Web/JSONView.h"
+
+namespace MQ
+{
+namespace Web
+{
+
+ClusterQueueManagerController::ClusterQueueManagerController() : MQController()
+{
+}
+
+
+ClusterQueueManagerController::~ClusterQueueManagerController()
+{
+
+}
+
+
+void ClusterQueueManagerController::inquire()
+{
+	Poco::JSON::Object::Ptr filter = new Poco::JSON::Object();
+
+	std::vector<std::string> parameters = getParameters();
+	// First parameter is queuemanager
+	// Second parameter is a clustername
+	if ( parameters.size() > 1 )
+	{
+		filter->set("clusterName", parameters[1]);
+	}
+	else
+	{
+		std::string clusterNameField = form().get("clusterName", "*");
+		filter->set("clusterName", clusterNameField.empty() ? "*" : clusterNameField);
+		std::string clusterQmgrNameField = form().get("clusterQmgrName", "*");
+		filter->set("clusterQmgrName", clusterQmgrNameField.empty() ? "*" : clusterQmgrNameField);
+		std::string channelNameField = form().get("channelName", "*");
+		filter->set("channelName", channelNameField.empty() ? "*" : channelNameField);
+	}
+
+	ClusterQueueManagerMapper mapper(*commandServer());
+	Poco::JSON::Array::Ptr clusqmgrs = mapper.inquire(filter);
+	set("clusqmgrs", clusqmgrs);
+	setView(new JSONView());
+}
+
+
+} } // Namespace MQ::Web
---
+++ MQWeb/src/ClusterQueueManagerMapper.cpp
@@ -0,0 +1,100 @@
+/*
+ * Copyright 2010 MQWeb - Franky Braem
+ *
+ * Licensed under the EUPL, Version 1.1 or  as soon they
+ * will be approved by the European Commission - subsequent
+ * versions of the EUPL (the "Licence");
+ * You may not use this work except in compliance with the
+ * Licence.
+ * You may obtain a copy of the Licence at:
+ *
+ * http://joinup.ec.europa.eu/software/page/eupl
+ *
+ * Unless required by applicable law or agreed to in
+ * writing, software distributed under the Licence is
+ * distributed on an "AS IS" basis,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied.
+ * See the Licence for the specific language governing
+ * permissions and limitations under the Licence.
+ */
+#include "MQ/Web/ClusterQueueManagerMapper.h"
+#include "MQ/MQException.h"
+
+#include "Poco/JSON/Object.h"
+
+namespace MQ {
+namespace Web {
+
+
+ClusterQueueManagerMapper::ClusterQueueManagerMapper(CommandServer& commandServer) : MQMapper(commandServer)
+{
+}
+
+ClusterQueueManagerMapper::~ClusterQueueManagerMapper()
+{
+}
+
+
+void ClusterQueueManagerMapper::change(const Poco::JSON::Object::Ptr&obj)
+{
+	poco_assert_dbg(false); // Not yet implemented
+}
+
+
+void ClusterQueueManagerMapper::create(const Poco::JSON::Object::Ptr& obj, bool replace)
+{
+  poco_assert_dbg(false); // Not yet implemented
+}
+
+
+void ClusterQueueManagerMapper::copy(const Poco::JSON::Object::Ptr& obj, bool replace)
+{
+	poco_assert_dbg(false); // Not yet implemented
+}
+
+
+Poco::JSON::Array::Ptr ClusterQueueManagerMapper::inquire(const Poco::JSON::Object::Ptr& filter)
+{
+	poco_assert_dbg(!filter.isNull());
+
+	Poco::JSON::Array::Ptr clusqmgrs = new Poco::JSON::Array();
+
+	PCF::Ptr inquireClusterQMgr = _commandServer.createCommand(MQCMD_INQUIRE_CLUSTER_Q_MGR);
+
+	if ( filter->has("clusterName") )
+	{
+		inquireClusterQMgr->addParameter(MQCA_CLUSTER_NAME, filter->optValue<std::string>("name", "*"));
+	}
+
+	if ( filter->has("channelName") )
+	{
+		inquireClusterQMgr->addParameter(MQCACH_CHANNEL_NAME, filter->optValue<std::string>("channelName", "*"));
+	}
+
+	if ( filter->has("clusterQmgrName") )
+	{
+		inquireClusterQMgr->addParameter(MQCA_CLUSTER_Q_MGR_NAME, filter->optValue<std::string>("clusterQmgrName", "*"));
+	}
+
+	PCF::Vector commandResponse;
+	_commandServer.sendCommand(inquireClusterQMgr, commandResponse);
+
+	for(PCF::Vector::iterator it = commandResponse.begin(); it != commandResponse.end(); it++)
+	{
+		if ( (*it)->getReasonCode() != MQRC_NONE ) // Skip errors (2035 not authorized for example)
+			continue;
+
+		if ( (*it)->isExtendedResponse() ) // Skip extended response
+			continue;
+
+		Poco::JSON::Object::Ptr clusqmgr = new Poco::JSON::Object();
+		clusqmgrs->add(clusqmgr);
+
+		mapToJSON(**it, clusqmgr);
+	}
+
+	return clusqmgrs;
+}
+
+}} //  Namespace MQ::Web
