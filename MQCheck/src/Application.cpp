70ab56b942f8b7bfe57e3e921d81bf939d3ee08f
fbraem <franky.braem@gmail.com>
2014-07-08 17:06:23 +0200

    Use - instead of unicode character in license text

--- MQCheck/src/Application.cpp
+++ MQCheck/src/Application.cpp
@@ -1,7 +1,7 @@
 /*
  * Copyright 2010 MQCheck - Franky Braem
  *
- * Licensed under the EUPL, Version 1.1 or  as soon they
+ * Licensed under the EUPL, Version 1.1 or - as soon they
  * will be approved by the European Commission - subsequent
  * versions of the EUPL (the "Licence");
  * You may not use this work except in compliance with the
