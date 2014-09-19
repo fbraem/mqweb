182b4b384372745f57d89084181a6964aee5ff72
fbraem <franky.braem@gmail.com>
2014-09-19 19:23:02 +0200

    Add queue_inq.php

--- samples/pygmentize_samples.sh
+++ samples/pygmentize_samples.sh
@@ -4,5 +4,6 @@ if [ -z "$1" ]
     echo "I need one argument: the root folder of the MQWeb GitHub pages"
     exit
 fi
-pygmentize -o $1/api/samples/queue_inq.html ./queue_inq.pl
-pygmentize -o $1/api/samples/channel_inq.html ./channel_inq.pl
+pygmentize -o $1/api/samples/queue_inq.pl.html ./queue_inq.pl
+pygmentize -o $1/api/samples/queue_inq.php.html ./queue_inq.php
+pygmentize -o $1/api/samples/channel_inq.pl.html ./channel_inq.pl
