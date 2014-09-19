67a43f8273e1169cfe62691223e6b834f3f3b60e
fbraem <franky.braem@gmail.com>
2014-09-19 22:57:30 +0200

    Use JSON library

--- samples/authrec_inq.pl
+++ samples/authrec_inq.pl
@@ -1,19 +1,27 @@
 #!/usr/bin/perl
 use strict;
 use warnings;
+use JSON;
 use LWP::UserAgent;
 use HTTP::Request::Common;
 
 my $qmgr = shift;
 die("Please pass me the name of a queuemanager as argument") unless defined($qmgr);
 
-my $json = '{ "ProfileName" : "", "Options" : ["Name All Matching", "Entity Explicit"]  }';
+my $json = JSON->new;
+
+my %input = ( 
+	'Options' => [ 
+		'Name All Matching', 
+		'Entity Explicit'
+	]
+);
 
 my $ua = LWP::UserAgent->new;
 my $req = POST 'http://localhost:8081/api/authrec/inquire/' . $qmgr;
 $req->header( 'Content-Type' => 'application/json' );
-$req->content($json);
+$req->content($json->encode(\%input));
 
 my $res = $ua->request($req);
 
-print $res->content();
+print $json->pretty->encode($json->decode($res->content()));
