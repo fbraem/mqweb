22834a82d61bac2b8dad7139d0afbf3c1bd3d4f2
fbraem <franky.braem@gmail.com>
2014-09-19 22:57:15 +0200

    Add authinfo_inq.php and authreq_inq.pl samples

--- samples/pygmentize_samples.sh
+++ samples/pygmentize_samples.sh
@@ -4,6 +4,9 @@ if [ -z "$1" ]
     echo "I need one argument: the root folder of the MQWeb GitHub pages"
     exit
 fi
+
+pygmentize -o $1/api/samples/authinfo_inq.php.html ./authinfo_inq.php
+pygmentize -o $1/api/samples/authrec_inq.pl.html ./authrec_inq.pl
 pygmentize -o $1/api/samples/queue_inq.pl.html ./queue_inq.pl
 pygmentize -o $1/api/samples/queue_inq.php.html ./queue_inq.php
 pygmentize -o $1/api/samples/channel_inq.pl.html ./channel_inq.pl
