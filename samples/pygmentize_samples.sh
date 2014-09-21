ba495c3eaa698904ba9ff2f48ad29f852cd8190d
fbraem <franky.braem@gmail.com>
2014-09-21 22:22:29 +0200

    Add more samples

--- samples/pygmentize_samples.sh
+++ samples/pygmentize_samples.sh
@@ -6,7 +6,10 @@ if [ -z "$1" ]
 fi
 
 pygmentize -o $1/api/samples/authinfo_inq.php.html ./authinfo_inq.php
+pygmentize -o $1/api/samples/authinfo_inq.pl.html ./authinfo_inq.pl
+pygmentize -o $1/api/samples/authrec_inq.php.html ./authrec_inq.php
 pygmentize -o $1/api/samples/authrec_inq.pl.html ./authrec_inq.pl
 pygmentize -o $1/api/samples/queue_inq.pl.html ./queue_inq.pl
 pygmentize -o $1/api/samples/queue_inq.php.html ./queue_inq.php
 pygmentize -o $1/api/samples/channel_inq.pl.html ./channel_inq.pl
+pygmentize -o $1/api/samples/channel_inq.php.html ./channel_inq.php
