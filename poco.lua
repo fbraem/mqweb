ba650a34192fe3135607c02ebedfc5e2ef04e1cf
fbraem <franky.braem@gmail.com>
2014-10-02 18:43:44 +0200

    Reorganize samples

--- poco.lua
+++ poco.lua
@@ -12,8 +12,8 @@ poco_dir = ""
 poco_lib_dir = poco_dir .. ""
 
 -- A Linux example:
---poco_dir = "/home/bronx/Development/poco" -- where did you extract POCO?
---poco_lib_dir = poco_dir .. "/static/lib/Linux/i686" -- where are the libraries?
+poco_dir = "/home/bronx/Development/poco" -- where did you extract POCO?
+poco_lib_dir = poco_dir .. "/static/lib/Linux/i686" -- where are the libraries?
 
 -- A Windows example:
 --poco_dir = "c:\\development\\poco-1.5.2-release"
--- samples/pygmentize_samples.sh
+++ samples/pygmentize_samples.sh
@@ -5,17 +5,28 @@ if [ -z "$1" ]
     exit
 fi
 
-pygmentize -o $1/api/samples/authinfo_inq.php.html ./authinfo_inq.php
-pygmentize -o $1/api/samples/authinfo_inq.pl.html ./authinfo_inq.pl
-pygmentize -o $1/api/samples/authrec_inq.php.html ./authrec_inq.php
-pygmentize -o $1/api/samples/authrec_inq.pl.html ./authrec_inq.pl
-pygmentize -o $1/api/samples/queue_inq.pl.html ./queue_inq.pl
-pygmentize -o $1/api/samples/queue_inq.php.html ./queue_inq.php
-pygmentize -o $1/api/samples/channel_inq.pl.html ./channel_inq.pl
-pygmentize -o $1/api/samples/channel_inq.php.html ./channel_inq.php
-pygmentize -o $1/api/samples/chstatus_inq.pl.html ./chstatus_inq.pl
-pygmentize -o $1/api/samples/chstatus_inq.php.html ./chstatus_inq.php
-pygmentize -o $1/api/samples/clusqmgr_inq.pl.html ./clusqmgr_inq.pl
-pygmentize -o $1/api/samples/clusqmgr_inq.php.html ./clusqmgr_inq.php
-pygmentize -o $1/api/samples/conn_inq.pl.html ./conn_inq.pl
-pygmentize -o $1/api/samples/conn_inq.php.html ./conn_inq.php
+pygmentize -o $1/api/samples/php/authinfo_inq.html ./php/authinfo_inq.php
+pygmentize -o $1/api/samples/perl/authinfo_inq.html ./perl/authinfo_inq.pl
+pygmentize -o $1/api/samples/php/authrec_inq.html ./php/authrec_inq.php
+pygmentize -o $1/api/samples/perl/authrec_inq.html ./perl/authrec_inq.pl
+pygmentize -o $1/api/samples/perl/queue_inq.html ./perl/queue_inq.pl
+pygmentize -o $1/api/samples/php/queue_inq.html ./php/queue_inq.php
+pygmentize -o $1/api/samples/perl/channel_inq.html ./perl/channel_inq.pl
+pygmentize -o $1/api/samples/php/channel_inq.html ./php/channel_inq.php
+pygmentize -o $1/api/samples/perl/chstatus_inq.html ./perl/chstatus_inq.pl
+pygmentize -o $1/api/samples/php/chstatus_inq.html ./php/chstatus_inq.php
+pygmentize -o $1/api/samples/perl/clusqmgr_inq.html ./perl/clusqmgr_inq.pl
+pygmentize -o $1/api/samples/php/clusqmgr_inq.html ./php/clusqmgr_inq.php
+pygmentize -o $1/api/samples/perl/conn_inq.html ./perl/conn_inq.pl
+pygmentize -o $1/api/samples/php/conn_inq.html ./php/conn_inq.php
+pygmentize -o $1/api/samples/perl/listener_inq.html ./perl/listener_inq.pl
+pygmentize -o $1/api/samples/php/listener_inq.html ./php/listener_inq.php
+pygmentize -o $1/api/samples/perl/namelist_inq.html ./perl/namelist_inq.pl
+pygmentize -o $1/api/samples/php/namelist_inq.html ./php/namelist_inq.php
+pygmentize -o $1/api/samples/perl/process_inq.html ./perl/process_inq.pl
+pygmentize -o $1/api/samples/php/process_inq.html ./php/process_inq.php
+pygmentize -o $1/api/samples/perl/service_inq.html ./perl/service_inq.pl
+pygmentize -o $1/api/samples/php/service_inq.html ./php/service_inq.php
+pygmentize -o $1/api/samples/perl/qmstatus.html ./perl/qmstatus_inq.pl
+pygmentize -o $1/api/samples/php/qmstatus.html ./php/qmstatus_inq.php
+
