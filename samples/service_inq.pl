f7487190bb16c233f2622c279227699b2166e11e
Franky Braem <franky.braem@gmail.com>
2014-10-02 13:07:45 +0200

    Create new file

---
+++ samples/service_inq.pl
@@ -0,0 +1,25 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use JSON;
+use LWP::UserAgent;
+use HTTP::Request::Common;
+
+my $qmgr = shift;
+die("Please pass me the name of a queuemanager as argument") unless defined($qmgr);
+
+my $json = JSON->new;
+
+my %input = ( 
+	'ServiceName' => '*',
+	'ServiceAttrs' => [ 'ServiceName' ]
+);
+
+my $ua = LWP::UserAgent->new;
+my $req = POST 'http://localhost:8081/api/service/inquire/' . $qmgr;    
+$req->header( 'Content-Type' => 'application/json' );
+$req->content($json->encode(\%input));
+
+my $res = $ua->request($req);
+
+print $json->pretty->encode($json->decode($res->content()));
