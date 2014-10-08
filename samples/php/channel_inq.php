47a1ae40e9cdc21b365cd39d715039a9b7eab61b
Franky Braem <franky.braem@gmail.com>
2014-10-08 15:11:33 +0200

    Make the example more realistic ...

--- samples/php/channel_inq.php
+++ samples/php/channel_inq.php
@@ -1,15 +1,36 @@
 <?php
 
 	/*
-	 * Inquire all SYSTEM channels from queuemanager PIGEON.
+	 * Inquire all client channels from queuemanager PIGEON.
 	 * MQWeb runs on localhost and is listening on port 8081. 
 	 */
-	$url = "http://localhost:8081/api/channel/inquire/PIGEON/SYSTEM*";
+	$url = "http://localhost:8081/api/channel/inquire/PIGEON/*/Server-connection";
+
 	$curl = curl_init();
 	curl_setopt($curl, CURLOPT_URL, $url);
-	curl_setopt($curl, CURLOPT_HEADER, false);
 	curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-	curl_setopt($curl, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1);
-	$response = curl_exec($curl);
-	$data = json_decode($response, true);		
-	print_r($data);
+
+	if ( ($response = curl_exec($curl) ) === false ) {
+		$err = curl_error($curl);
+		echo "An HTTP error occurred while getting queue information: $err\n";
+	}
+	else {
+		$json = json_decode($response);
+		if ( isset($json->error) ) {
+			echo "An MQ error occurred while inquiring channels.\n";
+			echo "Reason Code: {$json->error->reason->code} - {$json->error->reason->desc}\n";
+		}
+		else {
+			if ( isset($json->channels) && count($json->channels) > 0 ) {
+				foreach($json->channels as $channel)
+				{
+					echo $channel->ChannelName->value;
+					echo "\n";
+				}
+			}
+			else
+			{
+				echo "No client channels found\n";
+			}
+		}
+	}
