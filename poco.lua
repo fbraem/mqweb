cb56908ed92481d4feaf0ed0e715a8996f556aa3
fbraem <franky.braem@gmail.com>
2014-11-04 21:44:17 +0100

    Using Poco 1.5.4

--- poco.lua
+++ poco.lua
@@ -12,10 +12,10 @@ poco_dir = ""
 poco_lib_dir = poco_dir .. ""
 
 -- A Linux example:
-poco_dir = "/home/bronx/Development/poco" -- where did you extract POCO?
-poco_lib_dir = poco_dir .. "/static/lib/Linux/i686" -- where are the libraries?
+-- poco_dir = "/home/bronx/Development/poco-1.5.4-all" -- where did you extract POCO?
+-- poco_lib_dir = poco_dir .. "/static/lib/Linux/i686" -- where are the libraries?
 
 -- A Windows example:
---poco_dir = "c:\\development\\poco-1.5.2-release"
+--poco_dir = "c:\\development\\poco-1.5.4-release"
 --poco_lib_dir = poco_dir .. "\\lib"
 
