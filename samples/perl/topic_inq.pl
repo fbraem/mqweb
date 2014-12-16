dd6ade61032e16a8106d2aaf2bda60a368158b48
fbraem <franky.braem@gmail.com>
2014-12-16 21:33:31 +0100

    Add inquire topic example for Perl

---
+++ samples/perl/topic_inq.pl
@@ -0,0 +1,49 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use feature qw(say);
+
+use JSON;
+use LWP::UserAgent;
+use HTTP::Request::Common;
+
+# This sample will show all SYSTEM topics from the given queuemanager.
+
+my $qmgr = shift;
+die("Please pass me the name of a queuemanager as argument") 
+	unless defined($qmgr);
+
+my $json = JSON->new;
+
+my %input = ( 
+	'TopicName' => 'SYSTEM*'
+);
+my $content = $json->encode(\%input);    
+
+my $ua = LWP::UserAgent->new;
+my $req = POST 'http://localhost:8081/api/topic/inquire/' . $qmgr;
+$req->header(
+	'Content-Type' => 'application/json',
+	'Content-length' => length($content)
+);
+$req->content($content);
+
+my $res = $ua->request($req);
+die $res->status_line unless $res->is_success;
+	
+my $mqweb = $json->decode($res->content());
+if ( exists($mqweb->{error}) ) {
+	say 'An MQ error occurred while inquiring topics.';
+	say 'Reason Code: '
+		, $mqweb->{error}->{reason}->{code}
+		, ' - '
+		, $mqweb->{error}->{reason}->{desc};
+}
+else {
+	foreach my $topic(@{$mqweb->{topics}}) {
+		my $output = $topic->{QName}->{value};
+		$output .= ' : ' . $topic->{CurrentQDepth}->{value} 
+			if ( exists($topic->{CurrentQDepth}) );
+		say $output;
+	}
+}
