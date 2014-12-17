08c70ee6552c11c378ed216b21b64d2ba8183ff3
Franky Braem <franky.braem@gmail.com>
2014-12-17 08:21:55 +0100

    Create tpstatus_inq.pl

---
+++ samples/perl/tpstatus_inq.pl
@@ -0,0 +1,46 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use feature qw(say);
+
+use JSON;
+use LWP::UserAgent;
+use HTTP::Request::Common;
+
+# This sample will show all topic status from the given queuemanager.
+
+my $qmgr = shift;
+die("Please pass me the name of a queuemanager as argument") 
+	unless defined($qmgr);
+
+my $json = JSON->new;
+
+my %input = ( 
+	'TopicString' => '*'
+);
+my $content = $json->encode(\%input);    
+
+my $ua = LWP::UserAgent->new;
+my $req = POST 'http://localhost:8081/api/tpstatus/inquire/' . $qmgr;
+$req->header(
+	'Content-Type' => 'application/json',
+	'Content-length' => length($content)
+);
+$req->content($content);
+
+my $res = $ua->request($req);
+die $res->status_line unless $res->is_success;
+	
+my $mqweb = $json->decode($res->content());
+if ( exists($mqweb->{error}) ) {
+	say 'An MQ error occurred while inquiring topic status.';
+	say 'Reason Code: '
+		, $mqweb->{error}->{reason}->{code}
+		, ' - '
+		, $mqweb->{error}->{reason}->{desc};
+}
+else {
+	foreach my $status(@{$mqweb->{statuses}}) {
+		say $status->{TopicName}->{value};
+	}
+}
