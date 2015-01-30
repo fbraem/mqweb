3c1ad725adc2d05e18e9e73b388fd9a0d48bc27a
fbraem <franky.braem@gmail.com>
2015-01-30 16:54:14 +0100

    Add sub/substatus samples

---
+++ samples/php/substatus_inq.php
@@ -0,0 +1,55 @@
+<?php
+/*
+ * This sample will show all subscription statuses from a queuemanager.
+ * MQWeb runs on localhost and is listening on port 8081. 
+ */
+if ( count($argv) < 2 ) {
+	echo 'Please pass a queuemanager name as argument', PHP_EOL;
+	exit;
+}
+
+$qmgr = $argv[1];
+
+$url = 'http://localhost:8081/api/sbstatus/inquire/' . $qmgr . '/*';
+
+$curl = curl_init();
+curl_setopt($curl, CURLOPT_URL, $url);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+
+if ( ($response = curl_exec($curl)) === false )	{
+	$err = curl_error($curl);
+	echo 'An HTTP error occurred while getting subscription status information from '
+		, $qmgr
+		, ': '
+		, $err
+		, PHP_EOL;
+}
+else {
+	$json = json_decode($response);
+	// When there is an error object returned, something went wrong with
+	// the WebSphere MQ command.
+	if ( isset($json->error) ) {
+		echo 'An MQ error occurred while inquiring subscription status from '
+			, $qmgr
+			, PHP_EOL;
+		echo 'Reason Code: '
+			,	$json->error->reason->code
+			, " - "
+			, $json->error->reason->desc
+			, PHP_EOL;
+	}
+	else {
+		echo 'Subscription Status on ', $qmgr, PHP_EOL;
+		echo '-----------------------', str_repeat('-', strlen($qmgr)), PHP_EOL;
+		if ( isset($json->statuses) && count($json->statuses) > 0 ) {
+			foreach($json->statuses as $status)
+			{
+				echo $status->SubName->value, PHP_EOL;
+			}
+		}
+		else
+		{
+			echo 'No status found', PHP_EOL;
+		}
+	}
+}
