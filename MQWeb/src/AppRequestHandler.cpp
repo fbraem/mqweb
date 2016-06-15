7a41d9752011f27d2a02c43ca4aed4f0d78f537b
fbraem <franky.braem@gmail.com>
2016-06-15 11:27:13 +0200

    Check mq.web.app configuration

--- MQWeb/src/AppRequestHandler.cpp
+++ MQWeb/src/AppRequestHandler.cpp
@@ -61,94 +61,91 @@ void AppRequestHandler::handleRequest(Poco::Net::HTTPServerRequest& request, Poc
 	std::string staticPathname = app.config().getString("mq.web.app", "");
 	if ( staticPathname.empty() )
 	{
-		response.setStatus(Poco::Net::HTTPResponse::HTTP_NOT_FOUND);
+		Poco::Logger& logger = Poco::Logger::get("mq.web");
+		logger.error("mq.web.app property not defined. Check your configuration.");
+		response.setStatus(Poco::Net::HTTPResponse::HTTP_INTERNAL_SERVER_ERROR);
 		response.send();
+		return;
 	}
 
-	if ( ! staticPathname.empty() )
+	Poco::Path staticPath(staticPathname);
+	staticPath.makeDirectory();
+
+	std::vector<std::string> uriPathSegments;
+	uri.getPathSegments(uriPathSegments);
+	std::vector<std::string>::iterator it = uriPathSegments.begin();
+	it++;
+	for(; it != uriPathSegments.end(); ++it)
 	{
-		Poco::Path staticPath(staticPathname);
-		staticPath.makeDirectory();
+		staticPath.append(*it);
+	}
 
-		std::vector<std::string> uriPathSegments;
-		uri.getPathSegments(uriPathSegments);
-		std::vector<std::string>::iterator it = uriPathSegments.begin();
-		it++;
-		for(; it != uriPathSegments.end(); ++it)
+	Poco::File staticFile(staticPath);
+
+	Poco::Logger& logger = Poco::Logger::get("mq.web.access");
+	if ( staticFile.exists() )
+	{
+		if ( !lastModifiedHeader.empty() )
 		{
-			staticPath.append(*it);
-		}
-
-		Poco::File staticFile(staticPath);
-
-		Poco::Logger& logger = Poco::Logger::get("mq.web.access");
-		if ( staticFile.exists() )
-		{
-			if ( !lastModifiedHeader.empty() )
+			Poco::DateTime lastModifiedDate;
+			int timeZoneDifferential = 0;
+			if ( Poco::DateTimeParser::tryParse(Poco::DateTimeFormat::HTTP_FORMAT, lastModifiedHeader, lastModifiedDate, timeZoneDifferential) )
 			{
-				Poco::DateTime lastModifiedDate;
-				int timeZoneDifferential = 0;
-				if ( Poco::DateTimeParser::tryParse(Poco::DateTimeFormat::HTTP_FORMAT, lastModifiedHeader, lastModifiedDate, timeZoneDifferential) )
+				if ( staticFile.getLastModified() <= lastModifiedDate.timestamp() )
 				{
-					if ( staticFile.getLastModified() <= lastModifiedDate.timestamp() )
-					{
-						logger.information(Poco::Logger::format("$0 : HTTP_NOT_MODIFIED", staticPath.toString()));
-						response.setStatus(Poco::Net::HTTPResponse::HTTP_NOT_MODIFIED);
-						response.send();
-						return;
-					}
+					logger.information(Poco::Logger::format("$0 : HTTP_NOT_MODIFIED", staticPath.toString()));
+					response.setStatus(Poco::Net::HTTPResponse::HTTP_NOT_MODIFIED);
+					response.send();
+					return;
 				}
 			}
-
-			logger.information(Poco::Logger::format("$0 : HTTP_OK", staticPath.toString()));
-
-			std::string mimeType;
-			if ( staticPath.getExtension().compare("gif") == 0 )
-			{
-				mimeType = "image/gif";
-			}
-			else if ( staticPath.getExtension().compare("css") == 0 )
-			{
-				mimeType = "text/css";
-			}
-			else if ( staticPath.getExtension().compare("html") == 0 || staticPath.getExtension().compare("htm") == 0)
-			{
-				mimeType = "text/html";
-			}
-			else if ( staticPath.getExtension().compare("js") == 0 )
-			{
-				mimeType = "text/javascript";
-			}
-			else if ( staticPath.getExtension().compare("png") == 0 )
-			{
-				mimeType = "image/png";
-			}
-			else if ( staticPath.getExtension().compare("jpg") == 0 || staticPath.getExtension().compare("jpeg") == 0)
-			{
-				mimeType = "image/jpeg";
-			}
-
-			try
-			{
-				response.sendFile(staticPath.toString(), mimeType);
-			}
-			catch(Poco::FileNotFoundException&)
-			{
-				// We can't get here normally ... but you never know :)
-				response.setStatusAndReason(Poco::Net::HTTPResponse::HTTP_NOT_FOUND, Poco::Logger::format("Can't find file $0", staticPath.toString()));
-			}
-			catch(Poco::OpenFileException&)
-			{
-				response.setStatusAndReason(Poco::Net::HTTPResponse::HTTP_INTERNAL_SERVER_ERROR, Poco::Logger::format("Can't open file $0", staticPath.toString()));
-			}
-			return;
 		}
-		else
+
+		logger.information(Poco::Logger::format("$0 : HTTP_OK", staticPath.toString()));
+
+		std::string mimeType;
+		if ( staticPath.getExtension().compare("gif") == 0 )
 		{
-			logger.error(Poco::Logger::format("$0 : HTTP_NOT_FOUND", staticFile.path()));
+			mimeType = "image/gif";
 		}
+		else if ( staticPath.getExtension().compare("css") == 0 )
+		{
+			mimeType = "text/css";
+		}
+		else if ( staticPath.getExtension().compare("html") == 0 || staticPath.getExtension().compare("htm") == 0)
+		{
+			mimeType = "text/html";
+		}
+		else if ( staticPath.getExtension().compare("js") == 0 )
+		{
+			mimeType = "text/javascript";
+		}
+		else if ( staticPath.getExtension().compare("png") == 0 )
+		{
+			mimeType = "image/png";
+		}
+		else if ( staticPath.getExtension().compare("jpg") == 0 || staticPath.getExtension().compare("jpeg") == 0)
+		{
+			mimeType = "image/jpeg";
+		}
+
+		try
+		{
+			response.sendFile(staticPath.toString(), mimeType);
+		}
+		catch(Poco::FileNotFoundException&)
+		{
+			// We can't get here normally ... but you never know :)
+			response.setStatusAndReason(Poco::Net::HTTPResponse::HTTP_NOT_FOUND, Poco::Logger::format("Can't find file $0", staticPath.toString()));
+		}
+		catch(Poco::OpenFileException&)
+		{
+			response.setStatusAndReason(Poco::Net::HTTPResponse::HTTP_INTERNAL_SERVER_ERROR, Poco::Logger::format("Can't open file $0", staticPath.toString()));
+		}
+		return;
 	}
 
+	logger.error(Poco::Logger::format("$0 : HTTP_NOT_FOUND", staticFile.path()));
 	response.setStatus(Poco::Net::HTTPResponse::HTTP_NOT_FOUND);
 	response.send();
 }
